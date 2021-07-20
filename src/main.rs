512a19b37786a7aba23b72d1d97bd05ec6265988
Gabriel Souza <gabrielsouza849@gmail.com>
2021-07-20 00:16:19 -0300

    chance lib to rdkafka

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,57 @@
-
-use kafka::client::FetchPartition;
-use std::borrow::Borrow;
-use std::ops::Deref;
-
-
-fn main() {
-
-//localhost 9092
-
-    let mut client = kafka::client::KafkaClient::new(vec!("localhost:9092".to_owned()));
-    client.load_metadata_all().unwrap();
-    //list topic
-    let topics: Vec<String> = client.topics().names().map(ToOwned::to_owned ).collect();
-    let partition =  topics.iter().map(|s|{
-        FetchPartition::new(s.as_str(),0,0)
-    }).collect::<Vec<FetchPartition>>();
-    //let a = &[FetchPartition::new()];
-    let result = client.fetch_messages(&partition[..]).unwrap();
-    //
-    for m in result {
-        for topic in m.topics() {
-            for partition in topic.partitions() {
-                match partition.data() {
-                    Err(e)=>{
-                        println!("error {:?}", e);
-                    },Ok(d)=>{
-                        for message in d.messages() {
+use rdkafka::config::{ClientConfig, RDKafkaLogLevel, FromClientConfig};
+use rdkafka::producer::{BaseRecord, BaseProducer};
+use rdkafka::consumer::{BaseConsumer, Consumer};
+use rdkafka::admin::{AdminClient, NewTopic, TopicReplication};
+use rdkafka::error::{RDKafkaErrorCode, KafkaError};
 
 
-                            println!("{:?}",   String::from_utf8_lossy(message.value));
-                        }
-                    }
-                }
-            }
+
+fn show_message(){
+
+    let mut r:BaseConsumer = ClientConfig::new()
+        .set("bootstrap.servers","localhost:9092")
+        .set("group.id","rust_example_group_1")
+        .set_log_level(RDKafkaLogLevel::Debug)
+        .create()
+        .unwrap();
+
+     r.subscribe(&vec!["topic-test"]).unwrap();
+    for c in r.iter() {
+        println!("{:?}", c.unwrap());
+    }
+}
+
+
+async fn create_topic(){
+    let mut client_config = ClientConfig::new();
+    client_config.set("bootstrap.servers","localhost:9092");
+    client_config.set("group.id","rust_example_group_1");
+    client_config.set_log_level(RDKafkaLogLevel::Debug);
+
+    let topic_config = NewTopic::new("topic_top",10,TopicReplication::Fixed(1));
+
+    let kfk_admin = AdminClient::from_config(&client_config).unwrap();
+
+    let result_create_topic = kfk_admin.create_topics([topic_config].iter(), &Default::default()).await;
+
+    match result_create_topic {
+        Ok(d) => {
+            println!("topic criado com sucesso {:#?}", d)
+        }
+        Err(d) => {
+            println!("erro ao  criar o topic {:#?}", d)
         }
     }
 
 
+}
 
-        /****.map(|topic| {
-            FetchPartition::new(&topic, 0, 0)
-        })
-        .collect::<Vec<FetchPartition>>();
-    //client.fetch_messages(topics_name.deref()).unwrap();
-    ****/
+fn send_message(){
+    unimplemented!()
+}
 
-
-
-
-
+#[tokio::main]
+async fn main() {
+    
+    create_topic().await;
 }
