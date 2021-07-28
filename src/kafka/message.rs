f8cd0bf17494b6cc1afe3d7d15845c6bf19b2235
Gabriel Souza <gabrielsouza849@gmail.com>
2021-07-28 01:03:23 -0300

    lendo todas as mensagens antigas e stream  de mensagem

--- src/kafka/message.rs
+++ src/kafka/message.rs
@@ -1,18 +1,21 @@
 use rdkafka::consumer::{StreamConsumer, Consumer};
 use rdkafka::config::FromClientConfig;
-use rdkafka::ClientConfig;
+use rdkafka::{ClientConfig, Message};
 use rdkafka::producer::{FutureProducer, FutureRecord};
-use rdkafka::message::{OwnedHeaders};
+use rdkafka::message::{OwnedHeaders, BorrowedMessage};
 use std::time::Duration;
 use futures::{StreamExt, TryStream};
 use futures::stream::FuturesUnordered;
 use rdkafka::util::{ Timeout};
-use rdkafka::error::KafkaResult;
+use rdkafka::error::{KafkaResult, KafkaError};
 
 
-pub  async fn consumer(topic_name: &str, client_config: &ClientConfig) {
+pub  async fn consumer_olds_messages(topic_name: &str, mut client_config: ClientConfig) {
 
-    let result_consumer:KafkaResult<StreamConsumer> =  client_config.create();
+    let result_consumer:KafkaResult<StreamConsumer> =  client_config
+        .set("auto.offset.reset","earliest" )
+        .set("group.id","read_all_msg_4" )
+        .create();
 
     match result_consumer {
         Ok(consumer)=> {
@@ -20,10 +23,75 @@ pub  async fn consumer(topic_name: &str, client_config: &ClientConfig) {
                 .subscribe(&[topic_name])
                 .expect("erro no subscribe");
 
-            consumer.stream().map(|result_msg|{
-                println!("Only print message {:#?}", result_msg);
-                None
-            }).collect::<Vec<Option<i16>>>();
+            loop {
+                let a = consumer.recv().await ;
+
+                match a {
+                    Ok(message) => {
+                            let empty = "empty".as_bytes();
+                            println!("message key {:?} message value {:?}",
+                                     String::from_utf8_lossy(message.key().unwrap_or(empty)),
+                                     String::from_utf8_lossy(message.payload().unwrap_or(empty))
+                            );
+                    }
+                    Err(e) => {
+                        println!("sem mensagem {:?}",e);
+                        break
+                    }
+                }
+            }
+
+
+            consumer.stream()
+                .map(|result_msg|{
+                    println!("Only print message {:#?}", result_msg);
+                    None
+                })
+                .collect::<Vec<Option<i16>>>()
+                .await;
+        },
+        Err(d)=> {
+            println!("Erro  client config {:#?}", d)
+
+        }
+
+    }
+}
+
+
+
+pub  async fn consumer(topic_name: &str, client_config: ClientConfig) {
+
+    let result_consumer:KafkaResult<StreamConsumer> =  client_config
+        .create();
+
+    match result_consumer {
+        Ok(consumer)=> {
+            consumer
+                .subscribe(&[topic_name])
+                .expect("erro no subscribe");
+            consumer.stream()
+                .map(|result_msg|{
+
+                    match result_msg  {
+                        Ok(msg) => {
+                            let empty = "empty".as_bytes();
+                            println!("message key {:?} message value {:?}",
+                                     String::from_utf8_lossy(msg.key().unwrap_or(empty)),
+                                     String::from_utf8_lossy(msg.payload().unwrap_or(empty))
+                            );
+                        }
+                        Err(e) => {
+                            println!("error receive message {:?}", e);
+                        }
+                    }
+
+
+
+                    None
+                })
+                .collect::<Vec<Option<i16>>>()
+                .await;
         },
         Err(d)=> {
             println!("Erro  client config {:#?}", d)
@@ -53,7 +121,7 @@ pub async fn send(topic_name :&str, key:String, message:String, client_config: &
                     Ok(format!("message saved in {:?} partition", partition_num))
                 }
                 Err((kfk_error,_)) => {
-                    Ok(format!("error message save  {:#?} ", kfk_error))
+                    Err(format!("error message save  {:#?} ", kfk_error))
                 }
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,33 @@
 mod kafka;
 use crate::kafka::client::default;
 use crate::kafka::topic::create_topic;
-use crate::kafka::message::{send,consumer};
+use crate::kafka::message::{send, consumer, consumer_olds_messages};
 
-use dotenv::dotenv;
+
 use uuid::v1::{Context, Timestamp};
 
 
 #[tokio::main]
 async fn main() {
-    dotenv();
+
 
     let topic_name  = option_env!("TOPIC_NAME").unwrap();
     let host  = option_env!("HOST").unwrap();
     let group_id  = option_env!("GROUP_ID").unwrap();
-    let client = default(host, group_id);
+    let mut client = default(host, group_id);
 
     create_topic(&client, 4,topic_name).await;
 
-    let context = Context::new(42);
-    let ts = Timestamp::from_unix(&context, 1497624119, 1234);
-    let uuid = uuid::Uuid::new_v1(ts, &[1, 2, 3, 4, 5, 6]).unwrap();
 
-    send(topic_name, uuid.to_string(), uuid::Uuid::new_v4().to_string(), &client).await;
 
-    consumer(topic_name, &client).await;
+    let result = send(topic_name, uuid::Uuid::new_v4().to_string(), uuid::Uuid::new_v4().to_string(), &client).await;
+
+    println!("{:#?}", result);
+
+    tokio::join!(
+        consumer(topic_name, default(host, group_id)),
+        consumer_olds_messages(topic_name, default(host, group_id))
+    );
 
 
 }
